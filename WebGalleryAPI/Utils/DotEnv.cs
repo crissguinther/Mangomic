83a16ad5360ab12911186df246c0cf3ddac82e38
Criss Guinther <crissguinther@proton.me>
2022-12-09 13:23:55 -0300

    base configuration

---
+++ WebGalleryAPI/Utils/DotEnv.cs
@@ -0,0 +1,29 @@
+﻿namespace WebGallery.Utils {
+    public class DotEnv {
+        public static void Config(string filePath) {
+            if (!File.Exists(filePath)) throw new IOException("File does not exist");
+
+            try {
+                using (var sr = new StreamReader(filePath)) {
+                    while (!sr.EndOfStream) {
+                        string[] line = sr.ReadLine().Split('=');
+                        if (line.Length != 2) continue;
+
+                        Environment.SetEnvironmentVariable(line[0], line[1]);
+
+                    }
+                }
+
+            } catch (IOException ex) {
+                Console.WriteLine("Error while reading file: " + ex.StackTrace);
+            }
+        }
+
+        public static void Config() {
+            var Root = Directory.GetCurrentDirectory();
+            var DotEnv = Path.Combine(Root, ".env");
+
+            Config(DotEnv);
+        }
+    }
+}
---
+++ WebGalleryContext/Context/WebGalleryContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebGallery.Context {
+    internal class WebGalleryContext : DbContext {
+        public WebGalleryContext(DbContextOptions<WebGalleryContext> options) : base(options) { 
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseMySQL($"Server={Environment.GetEnvironmentVariable("DB_HOST")};" +
+                $"UserID={Environment.GetEnvironmentVariable("DB_USER")};" +
+                $"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")};" +
+                $"Database={Environment.GetEnvironmentVariable("DB_DATABASE")}");
+        }
+    }
+}
